5e0d8ff4bcf7d0ab732a140926e6cf1d46a63406
Ysnsn <2746130558@qq.com>
2022-01-05 19:21:01 +0800

    2525255

--- other/active.js
+++ other/active.js
@@ -259,7 +259,7 @@ async function other() {
     */
    // await cfm2()
     console.log("粉丝福利任务开始,记得去app中首页分别搜索进行qq号绑定哦！！")
-   // await lottery("lottery", 29, [1, 2, 3, 4, 5, 6],"\n[517517]")
+    await lottery("lottery", 31, [1, 2, 3, 4, 5, 6,7,8],"\n[220105]")
 
   //  await lottery("lottery", 26, [1, 2, 3, 4, 5, 6,7],"\n[888288]")
    // await lottery("lottery", 23, [1, 2, 3, 4, 5, 6, 7],"\n[211021]")//79979
